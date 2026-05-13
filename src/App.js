9ca6f906e3aea9103c1c6f04a472021a06d738a3
nithya5688 <nithu.srini@gmail.com>
2026-05-13 10:47:07 -0600

    Testing AWS deployment to Ec2 test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         howdy? AWS test deployment to EC2
+         howdy? AWS test deployment to EC2 test
         </p>
         <a
           className="App-link"
